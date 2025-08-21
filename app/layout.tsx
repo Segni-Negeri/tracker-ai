878c0e15215512c4dd79502470f80bab44c60dd7
Segni-Negeri <segninegeri.dev@gmail.com>
2025-08-21 03:24:28 -0500

    Add clerk authenthication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "tracker-ai",
@@ -14,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body>{children}</body>
+        <body>
+          <Navbar></Navbar>
+          {children}
+        </body>
     </html>
+    </ClerkProvider>
   );
 }
